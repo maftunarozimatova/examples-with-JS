35b7956d30134889b5174ea1210827c6bd2fae94
Maftuna <156097566+maftunarozimatova@users.noreply.github.com>
2024-03-04 16:55:29 +0500

    examples

---
+++ main.js
@@ -0,0 +1,79 @@
+// Task 1
+
+let a = +prompt("birinchi raqam")
+let b = +prompt("ikkinchi raqam")
+let c = +prompt("javob")
+
+let d = a + b +c
+
+if (c === d) {
+    console.log("True");
+} else {
+    console.log("noto'g'ri");
+}
+
+// Task 2
+
+let e = +prompt("birinchi raqam")
+let f = +prompt("ikkinchi raqam")
+let j = +prompt("uchinchi raqam")
+let i = +prompt("javob")
+
+let k = (e - j) + f
+
+if (i === k) {
+    console.log("True");
+} else {
+    console.log("noto'g'ri");
+}
+
+// Task 3
+
+let l = +prompt("birinchi raqam")
+let m = +prompt("ikkinchi raqam")
+let n = +prompt("uchinchi raqam")
+let o = +prompt("to'rtinchi raqam")
+
+let p = (l * m) - (n / o)
+
+if (o === p) {
+    console.log("True");
+} else {
+    console.log("noto'g'ri");
+}
+
+// Task 4
+
+let q = +prompt("birinchi raqam")
+let r = +prompt("ikkinchi raqam")
+let s = +prompt("uchinchi raqam")
+let t = +prompt("to'rtinchi raqam")
+let u = +prompt("beshinchi raqam")
+
+let v = (q / u) + (r - t) * u
+
+if (u === v) {
+    console.log("True");
+} else {
+    console.log("noto'g'ri");
+}
+
+// Task 5
+
+let abc = +prompt("1-raqam")
+let acb = +prompt("2-raqam")
+let bca = +prompt("3-raqam")
+let www = +prompt("4-raqam")
+let sss = +prompt("5-raqam")
+let yyy = +prompt("6-raqam")
+let hhh = +prompt("7-raqam")
+let mmm = +prompt("javob")
+
+let nnn = (abc - acb) + (bca + www - sss) * (yyy / hhh) 
+
+if (mmm === nnn) {
+   console.log("To'g'ri"); 
+} else {
+    console.log("noto'g'ri");
+}
+
